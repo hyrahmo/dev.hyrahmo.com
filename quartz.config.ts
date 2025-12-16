af28886004af870916783136d843bc859b69bc5b
Hyrahmo <229695956+hyrahmo@users.noreply.github.com>
2025-12-17 02:47:13 +0300

    Quartz sync: Dec 17, 2025, 2:47 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "hyrahmo ",
+    pageTitleSuffix: "dev",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dev.hyrahmo.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
